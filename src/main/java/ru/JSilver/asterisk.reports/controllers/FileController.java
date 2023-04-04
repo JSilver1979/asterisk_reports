b9f38f1f3af60a0868793781ffa7fd6344278dd0
Dmitry Kukolev <glarosh@gmail.com>
2023-04-04 12:13:58 +0300

    Added method for detailed stats in DownloadReportService

--- src/main/java/ru/JSilver/asterisk.reports/controllers/FileController.java
+++ src/main/java/ru/JSilver/asterisk.reports/controllers/FileController.java
@@ -27,4 +27,16 @@ public class FileController {
         httpHeaders.setCacheControl(CacheControl.noCache().getHeaderValue());
         return new ResponseEntity<>(isr,httpHeaders, HttpStatus.OK);
     }
+
+    @PostMapping("/get_reportV3")
+    public ResponseEntity<InputStreamResource> downloadDetailedFile(@RequestBody DateSearchDto searchDto) {
+        byte[] buf = downloadService.getDetailedStrings(searchDto.getYear(), searchDto.getMonth(), searchDto.getGroup()).getBytes();
+
+        InputStreamResource isr = new InputStreamResource(new ByteArrayInputStream(buf));
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.valueOf("text/csv"));
+        httpHeaders.setContentLength(buf.length);
+        httpHeaders.setCacheControl(CacheControl.noCache().getHeaderValue());
+        return new ResponseEntity<>(isr,httpHeaders, HttpStatus.OK);
+    }
 }
--- src/main/java/ru/JSilver/asterisk.reports/services/DownloadReportService.java
+++ src/main/java/ru/JSilver/asterisk.reports/services/DownloadReportService.java
@@ -1,9 +1,9 @@
 package ru.JSilver.asterisk.reports.services;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.JSilver.asterisk.reports.dto.CallItemDto;
+import ru.JSilver.asterisk.reports.dto.CallQueueDto;
 
 import java.time.LocalDateTime;
 import java.time.YearMonth;
@@ -13,14 +13,13 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class DownloadReportService {
     private final CallsService callsService;
 
+    private final CallServiceV3 callServiceV3;
+
     public String getSomeData(int year, int month, String group) {
-        LocalDateTime searchFromDate = LocalDateTime.of(year,month,1,0,0,1);
-        LocalDateTime searchToDate = LocalDateTime.of(year,month, YearMonth.of(year,month).atEndOfMonth().getDayOfMonth(),23,59,59);
-        List<CallItemDto> callItems = callsService.getCallItems(searchFromDate, searchToDate, group)
+        List<CallItemDto> callItems = callsService.getCallItems(getStartDate(year, month), getEndDate(year, month), group)
                 .stream()
                 .sorted(Comparator.comparing(CallItemDto::getCallDate).thenComparing(CallItemDto::getCallTime))
                 .collect(Collectors.toList());
@@ -32,6 +31,46 @@ public class DownloadReportService {
             sb.append(item.toString()).append("\n");
         }
         return sb.toString();
+    }
+
+    public String getDetailedStrings(int year, int month, String queue) {
+        String header = "Call ID;Call Number;Call Date;Call Time;Queue Time;Wait Time;Agent Number;Answer Time;Duration;Call Status;Queue Status;Redirected\n";
+        String delimiter = ";";
+        List<CallQueueDto> list = callServiceV3.getDetailedCalls(getStartDate(year, month), getEndDate(year,month), queue)
+                .stream()
+                .sorted(Comparator.comparing(CallQueueDto::getDate).thenComparing(CallQueueDto::getCallTime))
+                .collect(Collectors.toList());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(header);
+        for (CallQueueDto item: list) {
+            sb
+                    .append(item.getCallId()).append(delimiter)
+                    .append(item.getNumber()).append(delimiter)
+                    .append(nullToString(item.getDate())).append(delimiter)
+                    .append(nullToString(item.getCallTime())).append(delimiter)
+                    .append(nullToString(item.getQueueTime())).append(delimiter)
+                    .append(nullToString(item.getWaitTime())).append(delimiter)
+                    .append(nullToString(item.getAgentNumber())).append(delimiter)
+                    .append(nullToString(item.getAnswerTime())).append(delimiter)
+                    .append(nullToString(item.getAnswerDuration())).append(delimiter)
+                    .append(item.getCallStatus()).append(delimiter)
+                    .append(item.getQueueStatus()).append(delimiter)
+                    .append((item.isRedirected() ? "REDIRECTED" : "")).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private String nullToString(Object o) {
+        return o == null ? "" : o.toString();
+    }
+    private LocalDateTime getStartDate(int year, int month) {
+        return LocalDateTime.of(year, month, 1, 0,0,1);
+    }
+
+    private LocalDateTime getEndDate(int year, int month) {
+        return LocalDateTime.of(year, month, YearMonth.of(year, month).atEndOfMonth().getDayOfMonth(), 23,59,59);
     }
 
 }
